e0cd2a6e90992a8c438fc314b9d1d060a4e60145
root <ericvazdez@email.com>
2024-03-29 17:18:10 -0600

    [Eventos] (ToDoForm) Manejo de eventos para el formulario

--- src/ToDoForm/index.js
+++ src/ToDoForm/index.js
@@ -1,21 +1,45 @@
 import React from "react";
+import { ToDoContext } from '../ToDoContext'
 import './ToDoForm.css'
 
 function ToDoForm(){
+    const {
+        setOpenModal,
+        addToDo
+    } = React.useContext(ToDoContext)
+
+    const [newToDoValue, setNewToDoValue] = React.useState('')
+
+    const onSubmit = (event) => {
+        event.preventDefault()
+        addToDo(newToDoValue)
+        setOpenModal(false)
+    }
+
+    const onCancel = () =>{
+        setOpenModal(false)
+    }
+
+    const onChange = (event) =>{
+        setNewToDoValue(event.target.value)
+    }
     return (
-        <form onSubmit={(event) => {
-                event.preventDefault()
-            }
-        }>
+        <form onSubmit={onSubmit}
+        >
             <label>
                 Escribe un nuevo ToDo
             </label>
             <textarea
+                value={newToDoValue}
+                onChange={onChange}
                 placeholder="Cortar Cebolla"
+                required
             ></textarea>
             <div className="ToDoForm-buttonContainer">
-                <button 
-                    className="ToDoForm-button ToDoForm-button--cancel">
+                <button
+                    type="button" 
+                    className="ToDoForm-button ToDoForm-button--cancel"
+                    onClick={onCancel}>
                     Cancelar
                 </button>
                 <button 
